f5a33bedc2dffe52dbbcf0d8fbd84eab5a2f02c4
Gilad Goldberg <ggoldberg@paypal.com>
2016-06-21 14:27:34 +0300

    step 3

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,20 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+	Image
 } from 'react-native';
 
 class VanityApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+				<View style={styles.bgWrapper}>
+					<Image style={styles.backgroundImage} source={require('./bg.png')} />
+				</View>
+        <Text style={styles.title}>
           ALL YOUR $$$ ARE BELONG TO US
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+				</Text>
       </View>
     );
   }
@@ -36,18 +33,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+		backgroundColor: 'transparent'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+	backgroundImage: {
+		resizeMode: 'cover',
+		flex: 1,
   },
+	bgWrapper: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0
+	}
 });
 
 AppRegistry.registerComponent('VanityApp', () => VanityApp);
